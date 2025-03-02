d468abcaab6733066c8f86b0e2db16f0a19f01e6
kokoszanelinio <118772128+kokoszanelinio@users.noreply.github.com>
2025-03-02 19:21:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fetch = require("node-fetch"); // v2.x
-
 const path = require("path");
 const app = express();
 
@@ -10,7 +9,6 @@ app.use(express.static(path.join(__dirname, "public")));
 // Endpoint /api/all – 3, 5, 114 w jednej paczce
 app.get("/api/all", async (req, res) => {
   try {
-    // Jedno wywołanie do mpk.wroc.pl
     const resp = await fetch("https://mpk.wroc.pl/bus_position", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -20,9 +18,8 @@ app.get("/api/all", async (req, res) => {
         "busList[bus][]": "114"
       })
     });
-    const data = await resp.json(); 
+    const data = await resp.json();
     // data = [{name: "3", type: "tram", x: 51..., y: 17...}, {...}, {...}]
-
     res.json(data);
   } catch (err) {
     console.error(err);
